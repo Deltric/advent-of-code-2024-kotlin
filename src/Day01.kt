c061015158d9cac1a67029cc78c30d8d7af8b6a3
Deltric <d3ltr1c@gmail.com>
2024-12-01 06:07:03 -0600

    Minor improvements to Day 1 solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,47 +1,43 @@
-import kotlin.math.max
-import kotlin.math.min
-
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(sides: List<Pair<Int, Int>>): Int {
         val leftSide = mutableListOf<Int>()
         val rightSide = mutableListOf<Int>()
 
-        input.map { it.split("   ") }
-            .forEach {
-                leftSide.add(it[0].toInt())
-                rightSide.add(it[1].toInt())
-            }
+        sides.forEach {
+            leftSide.add(it.first)
+            rightSide.add(it.second)
+        }
 
         leftSide.sort()
         rightSide.sort()
 
         var totalDistance = 0
         for (index in leftSide.indices) {
-            val smallerNumber = min(leftSide[index], rightSide[index])
-            val biggerNumber = max(leftSide[index], rightSide[index])
-            totalDistance += biggerNumber - smallerNumber
+            val (smaller, bigger) = minMax(leftSide[index], rightSide[index])
+            totalDistance += bigger - smaller
         }
         return totalDistance
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(sides: List<Pair<Int, Int>>): Int {
         val leftSide = mutableListOf<Int>()
         val occurrences = mutableMapOf<Int, Int>()
 
-        input.map { it.split("   ") }
-            .forEach {
-                leftSide.add(it[0].toInt())
-                val rightNumber = it[1].toInt()
-                occurrences[rightNumber] = occurrences.getOrDefault(rightNumber, 0) + 1
-            }
+        sides.forEach {
+            leftSide.add(it.first)
+            occurrences.compute(it.second) { _, v -> (v ?: 0) + 1 }
+        }
 
         return leftSide.sumOf { it * occurrences.getOrDefault(it, 0) }
     }
 
-    val dayOneInput = readDay(1)
-    val part1Result = part1(dayOneInput)
-    println("Total Distance: $part1Result")
+    val sides = readDay(1)
+        .map { it.split("   ") }
+        .map { it[0].toInt() to it[1].toInt() }
 
-    val part2Result = part2(dayOneInput)
-    println("Similarity Score: $part2Result")
+    val totalDistance = part1(sides)
+    println("Total Distance: $totalDistance")
+
+    val similarityScore = part2(sides)
+    println("Similarity Score: $similarityScore")
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,11 +1,22 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.math.max
+import kotlin.math.min
 
+/**
+ * Shortcut for readInput that formats a day to file name for input.
+ */
 fun readDay(day: Int) = readInput("Day${day.toString().padStart(2, '0')}")
 
 /**
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = Path("./input/$name.txt").readText().trim().lines()
+
+/**
+ * Helper for min maxing two numbers.
+ * @return First number is the minimum, second number is maximum.
+ */
+fun minMax(a: Int, b: Int): Pair<Int, Int> {
+    return min(a, b) to max(a, b)
+}
