49ae806ff452cf46a619ea6bf4331f446647e248
Collin Morrison <d3ltr1c@gmail.com>
2024-12-02 01:21:16 -0600

    Initial solution for part one

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,32 @@
 fun main() {
-    fun part1(): Int {
-        TODO("Do part one.")
+    fun part1(reports: List<List<Int>>): Int {
+        return reports.count { report ->
+            var increase = true
+            var lastLevel = -1
+
+            report.forEachIndexed { index, level ->
+                if (index == 0) {
+                    lastLevel = level
+                    return@forEachIndexed
+                } else if (index == 1) {
+                    if (level < lastLevel) {
+                        increase = false
+                    }
+                }
+
+                if (lastLevel == level) {
+                    return@count false
+                } else if (increase && (level < lastLevel || level - lastLevel > 3)) {
+                    return@count false
+                } else if (!increase && (level > lastLevel || lastLevel - level > 3)) {
+                    return@count false
+                }
+
+                lastLevel = level
+            }
+
+            true
+        }
     }
 
     fun part2(): Int {
@@ -8,7 +34,8 @@ fun main() {
     }
 
     val reports = readDay(2)
+        .map { it.split(" ").map { level -> level.toInt() } }
 
-    val safeReports = part1()
+    val safeReports = part1(reports)
     println("Safe Reports: $safeReports")
 }
