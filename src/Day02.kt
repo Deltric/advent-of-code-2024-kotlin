f156d31d7b1690034ee941aa272861a6d81cdb1c
Collin Morrison <d3ltr1c@gmail.com>
2024-12-02 02:38:26 -0600

    Refactor part one to use checkReport method

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,36 +1,6 @@
 fun main(args: Array<String>) {
     val sampleMode = args.contains("sampleMode")
 
-    fun part1(reports: List<List<Int>>): Int {
-        return reports.count { report ->
-            var increase = true
-            var lastLevel = -1
-
-            report.forEachIndexed { index, level ->
-                if (index == 0) {
-                    lastLevel = level
-                    return@forEachIndexed
-                } else if (index == 1) {
-                    if (level < lastLevel) {
-                        increase = false
-                    }
-                }
-
-                if (lastLevel == level) {
-                    return@count false
-                } else if (increase && (level < lastLevel || level - lastLevel > 3)) {
-                    return@count false
-                } else if (!increase && (level > lastLevel || lastLevel - level > 3)) {
-                    return@count false
-                }
-
-                lastLevel = level
-            }
-
-            true
-        }
-    }
-
     fun checkReport(report: List<Int>): Boolean {
         var increase = true
         var lastLevel = -1
@@ -62,6 +32,10 @@ fun main(args: Array<String>) {
         }
 
         return true
+    }
+
+    fun part1(reports: List<List<Int>>): Int {
+        return reports.count(::checkReport)
     }
 
     fun part2(reports: List<List<Int>>): Int {
