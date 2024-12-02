cf22410ad0ff5c5e4e009e3e0922bd94500c0ceb
Deltric <d3ltr1c@gmail.com>
2024-12-01 23:23:39 -0600

    Setup Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,14 @@
+fun main() {
+    fun part1(): Int {
+        TODO("Do part one.")
+    }
+
+    fun part2(): Int {
+        TODO("Do part two.")
+    }
+
+    val reports = readDay(2)
+
+    val safeReports = part1()
+    println("Safe Reports: $safeReports")
+}
