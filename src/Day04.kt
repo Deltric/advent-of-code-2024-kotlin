2aa98e1f8282f183209d82dcbf1c9947d4f4d5c8
Deltric <d3ltr1c@gmail.com>
2024-12-04 13:43:14 -0600

    Setup day 4

---
+++ src/Day04.kt
@@ -0,0 +1,11 @@
+fun main() {
+    fun part1(): Int {
+        TODO("Implement part one")
+    }
+
+    fun part2(): Int {
+        TODO("Implement part two")
+    }
+
+    val input = readDay(4)
+}
