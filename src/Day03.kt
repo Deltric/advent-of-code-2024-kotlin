e3be97c36ea3f2ce0875b203f89a172c4e46117d
Collin Morrison <d3ltr1c@gmail.com>
2024-12-03 22:09:52 -0600

    Setup day 3

---
+++ src/Day03.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    fun part1(reports: List<List<Int>>): Int {
+        TODO("Implement part 1")
+    }
+
+    fun part2(reports: List<List<Int>>): Int {
+        TODO("Implement part 2")
+    }
+
+    val input = readDay(3)
+}
